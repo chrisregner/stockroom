166e512d72af77f64f8aacfdf151e30b1bc3a6b3
Christopher Regner <chrisregner.com@gmail.com>
2024-02-16 05:07:09 +0800

    feat: add product

--- stockroom/ContentView.swift
+++ stockroom/ContentView.swift
@@ -9,53 +9,205 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Query private var products: [Product]
 
+//    var body: some View {
+//        NavigationSplitView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp, format: Date.FormatStyle`(date: .numeric, time: .standard))")
+//                    } label: {
+//                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//        } detail: {
+//            Text("Select an item")
+//        }
+//    }
+//
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(timestamp: Date())
+//            modelContext.insert(newItem)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        NavigationView {
+            VStack {
+                Spacer()
+                NavigationLink(destination: AddProductView()) {
+                    Text("Add Product")
+                }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                Spacer()
+                
+                List {
+                    ForEach(products) { product in
+                        HStack {
+                            Text(product.name)
+                            Spacer()
+                            Text(product.price, format: .currency(code: "PHP"))
+                            Text("QTY: \(String(product.stock))")
+                                .frame(width: 72, alignment: .leading)
+                                .padding(.leading, 8)
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            .navigationBarTitle("Home")
         }
     }
 }
 
+struct AddProductView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var name = ""
+    @State private var price = 0.0
+    @State private var stock = 0
+    @State private var description = ""
+    
+    @State private var isNameEmptyAlertPresented = false
+    
+    private var integerFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .none
+        formatter.maximumFractionDigits = 0 // Ensures no fractional part
+        return formatter
+    }
+    
+    private func addProduct() {
+        // withAnimation {
+            let newItem = Product(
+                timestamp: Date(),
+                name: name,
+                price: price,
+                stock: stock,
+                productDescription: description
+            )
+        
+            modelContext.insert(newItem)
+        // }
+    }
+    
+    var body: some View {
+        VStack {
+            Form {
+                VStack(alignment: .leading) {
+                    Text("Product Name:")
+                    TextField("Enter product name", text: $name)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                    
+                    Text("Price:")
+                    TextField(
+                        "Enter price",
+                        value: $price,
+                        format: .currency(code: "PHP")
+                    )
+                        .keyboardType(.decimalPad)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                    
+                    Text("Stock:")
+                    TextField("Enter stock", value: $stock, formatter: integerFormatter)
+                        .keyboardType(.numberPad)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                    
+                    Text("Description:")
+                    TextEditor(text: $description)
+                        .frame(height: 120)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                Button("Cancel") {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                
+                Spacer()
+                
+                Button("Save") {
+                    if !name.isEmpty {
+                        addProduct()
+                        
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        // Show an alert indicating that the product name is empty
+                        isNameEmptyAlertPresented = true
+                    }
+                }
+                    .alert(isPresented: $isNameEmptyAlertPresented) {
+                        Alert(
+                            title: Text("Error"),
+                            message: Text("Product name cannot be empty."),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+            }
+                .padding(.horizontal)
+        }
+        
+        .navigationBarTitle("Add Product")
+
+    }
+}
+
+let previewContainer: ModelContainer = {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Product.self, configurations: config)
+        
+        Task { @MainActor in
+            let context = container.mainContext
+            
+            for _ in 0..<10 {
+                let product = Product.createRandom()
+                context.insert(product)
+            }
+        }
+        
+        return container
+    } catch {
+        fatalError()
+    }
+}()
+
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(previewContainer)
+//    AddProductView()
+//        .modelContainer(for: Product.self, inMemory: true)
 }
---
+++ stockroom/Product.swift
@@ -0,0 +1,37 @@
+//
+//  Item.swift
+//  stockroom
+//
+//  Created by Christopher Regner on 2/16/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Product {
+    var timestamp: Date
+    var name: String
+    var price: Double
+    var stock: Int
+    var productDescription: String
+
+    init(timestamp: Date, name: String, price: Double, stock: Int, productDescription: String) {
+        self.timestamp = timestamp
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.productDescription = productDescription
+    }
+    
+    static func createRandom() -> Product {
+        let names = ["Product 1", "Product 2", "Product 3", "Product 4", "Product 5"]
+        let descriptions = ["Description 1", "Description 2", "Description 3", "Description 4", "Description 5"]
+        
+        let randomIndex = Int.random(in: 0..<names.count)
+        let randomPrice = Double.random(in: 1.0...100.0)
+        let randomStock = Int.random(in: 0...100)
+        
+        return Product(timestamp: Date(), name: names[randomIndex], price: randomPrice, stock: randomStock, productDescription: descriptions[randomIndex])
+    }
+}
