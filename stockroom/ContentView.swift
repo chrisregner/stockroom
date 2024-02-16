17a4ab384a8e4b11cacef210e0bf87815dd12f71
Christopher Regner <chrisregner.com@gmail.com>
2024-02-16 16:25:14 +0800

    feat: view and edit product

--- stockroom/ContentView.swift
+++ stockroom/ContentView.swift
@@ -11,67 +11,28 @@ import SwiftData
 struct ContentView: View {
     @Query private var products: [Product]
 
-//    var body: some View {
-//        NavigationSplitView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp, format: Date.FormatStyle`(date: .numeric, time: .standard))")
-//                    } label: {
-//                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-//                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//        } detail: {
-//            Text("Select an item")
-//        }
-//    }
-//
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(timestamp: Date())
-//            modelContext.insert(newItem)
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            for index in offsets {
-//                modelContext.delete(items[index])
-//            }
-//        }
-//    }
     var body: some View {
         NavigationView {
             VStack {
                 Spacer()
-                NavigationLink(destination: AddProductView()) {
+                NavigationLink(destination: ProductView()) {
                     Text("Add Product")
                 }
                     .buttonStyle(.borderedProminent)
                     .tint(.blue)
                 Spacer()
-                
+
                 List {
                     ForEach(products) { product in
-                        HStack {
-                            Text(product.name)
-                            Spacer()
-                            Text(product.price, format: .currency(code: "PHP"))
-                            Text("QTY: \(String(product.stock))")
-                                .frame(width: 72, alignment: .leading)
-                                .padding(.leading, 8)
+                        NavigationLink(destination: ProductView(product: product)) {
+                                HStack {
+                                    Text(product.name)
+                                    Spacer()
+                                    Text(product.price, format: .currency(code: "PHP"))
+                                    Text("QTY: \(String(product.stock))")
+                                        .frame(width: 72, alignment: .leading)
+                                        .padding(.leading, 8)
+                                }
                         }
                     }
                 }
@@ -81,26 +42,35 @@ struct ContentView: View {
     }
 }
 
-struct AddProductView: View {
+struct ProductView: View {
+    var product: Product?
+
     @Environment(\.modelContext) private var modelContext
     @Environment(\.presentationMode) var presentationMode
-    
-    @State private var name = ""
-    @State private var price = 0.0
-    @State private var stock = 0
-    @State private var description = ""
-    
+
+    @State private var name: String
+    @State private var price: Double
+    @State private var stock: Int
+    @State private var description: String
+
+    init(product: Product? = nil) {
+        self.product = product
+        _name = State(initialValue: product?.name ?? "")
+        _price = State(initialValue: product?.price ?? 0.0)
+        _stock = State(initialValue: product?.stock ?? 0)
+        _description = State(initialValue: product?.productDescription ?? "")
+    }
+
     @State private var isNameEmptyAlertPresented = false
-    
+
     private var integerFormatter: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .none
         formatter.maximumFractionDigits = 0 // Ensures no fractional part
         return formatter
     }
-    
+
     private func addProduct() {
-        // withAnimation {
             let newItem = Product(
                 timestamp: Date(),
                 name: name,
@@ -108,11 +78,34 @@ struct AddProductView: View {
                 stock: stock,
                 productDescription: description
             )
-        
+
             modelContext.insert(newItem)
-        // }
     }
-    
+
+    private func updateProduct() {
+        guard let product = product else { return }
+
+        product.name = name
+        product.price = price
+        product.stock = stock
+        product.productDescription = description
+    }
+
+    private func save() {
+        if name.isEmpty {
+            isNameEmptyAlertPresented = true
+            return
+        }
+
+        if (self.product != nil) {
+            updateProduct()
+        } else {
+            addProduct()
+        }
+
+        self.presentationMode.wrappedValue.dismiss()
+    }
+
     var body: some View {
         VStack {
             Form {
@@ -121,7 +114,7 @@ struct AddProductView: View {
                     TextField("Enter product name", text: $name)
                         .padding()
                         .border(Color.gray, width: 1)
-                    
+
                     Text("Price:")
                     TextField(
                         "Enter price",
@@ -131,13 +124,13 @@ struct AddProductView: View {
                         .keyboardType(.decimalPad)
                         .padding()
                         .border(Color.gray, width: 1)
-                    
+
                     Text("Stock:")
                     TextField("Enter stock", value: $stock, formatter: integerFormatter)
                         .keyboardType(.numberPad)
                         .padding()
                         .border(Color.gray, width: 1)
-                    
+
                     Text("Description:")
                     TextEditor(text: $description)
                         .frame(height: 120)
@@ -145,27 +138,20 @@ struct AddProductView: View {
                         .border(Color.gray, width: 1)
                 }
             }
-            
+
             Spacer()
-            
+
             HStack {
                 Button("Cancel") {
                     self.presentationMode.wrappedValue.dismiss()
                 }
                 .buttonStyle(.borderedProminent)
                 .tint(.red)
-                
+
                 Spacer()
-                
+
                 Button("Save") {
-                    if !name.isEmpty {
-                        addProduct()
-                        
-                        self.presentationMode.wrappedValue.dismiss()
-                    } else {
-                        // Show an alert indicating that the product name is empty
-                        isNameEmptyAlertPresented = true
-                    }
+                    save()
                 }
                     .alert(isPresented: $isNameEmptyAlertPresented) {
                         Alert(
@@ -179,7 +165,7 @@ struct AddProductView: View {
             }
                 .padding(.horizontal)
         }
-        
+
         .navigationBarTitle("Add Product")
 
     }
@@ -189,16 +175,16 @@ let previewContainer: ModelContainer = {
     do {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Product.self, configurations: config)
-        
+
         Task { @MainActor in
             let context = container.mainContext
-            
+
             for _ in 0..<10 {
                 let product = Product.createRandom()
                 context.insert(product)
             }
         }
-        
+
         return container
     } catch {
         fatalError()
